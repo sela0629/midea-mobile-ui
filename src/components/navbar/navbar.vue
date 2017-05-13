cb1c875c3f02d6ea2f176d35fa19dfa86a03e1c7
sela <598110274@qq.com>
2017-05-13 13:27:56 +0800

    navbar组件化
    将顶部导航组件化，但是还未处理panelHtml抽离。

--- src/components/navbar/navbar.vue
+++ src/components/navbar/navbar.vue
@@ -1,34 +1,13 @@
 <template>
-<!-- S  page -->
-  <div class="page navbar">
-    <!-- <div class="page__hd"> -->
-      
-    <!-- </div> -->
-
-   <!-- S  page__bd -->
-
-   <div class="page__bd">
-
-      <div class="midea-tab">
+<!-- {{navbar.arrItems}} -->
+      <div v-bind:class="['midea-tab',navbarData.diyClass]">
         <div class="midea-navbar">
-          <div v-bind:class="['midea-navbar__item',activeIndex == 1 ? 'midea-navbar__item_on' : '']" v-on:click="switchTab" data-index='1'>选项一</div>
-          <div v-bind:class="['midea-navbar__item',activeIndex == 2 ? 'midea-navbar__item_on' : '']" v-on:click="switchTab" data-index='2'>选项二</div>
-          <div v-bind:class="['midea-navbar__item',activeIndex == 3 ? 'midea-navbar__item_on' : '']" v-on:click="switchTab" data-index='3'>选项三</div>
-          <div v-bind:class="['midea-navbar__item',activeIndex == 4 ? 'midea-navbar__item_on' : '']" v-on:click="switchTab" data-index='4'>选项四</div>
+          <div v-for="(item, index) in navbarData.arrItems" v-bind:class="['midea-navbar__item',activeIndex == (index+1)? 'midea-navbar__item_on' : '']" v-on:click="switchTab" v-bind:data-index='index+1'>{{item.navName}}</div>
         </div>
         <div class="midea-tab__panel">
-           <div style="text-align:center;margin-top:50px;" v-bind:class="[activeIndex == 1 ? 'show' : 'hide']">选项面板1</div>
-           <div style="text-align:center;margin-top:50px;" v-bind:class="[activeIndex == 2 ? 'show'  : 'hide']">选项面板2</div>
-           <div style="text-align:center;margin-top:50px;" v-bind:class="[activeIndex == 3 ? 'show'  : 'hide']">选项面板3</div>
-                   <div style="text-align:center;margin-top:50px;" v-bind:class="[activeIndex == 4 ? 'show'  : 'hide']">选项面板4</div>
+           <div v-for="(item, index) in navbarData.arrItems"  style="text-align:center;margin-top:50px;" v-bind:class="[activeIndex == (index+1) ? 'show' : 'hide']" v-html="item.panelHtml"></div>
         </div>
       </div>
-
-   </div>
-
-   <!-- E  page__bd -->
-   <!-- E  page -->
- </div>
 
 </template>
 
@@ -40,6 +19,26 @@
         msg: 'navbar demo',
         activeIndex: 1
       }
+    },
+    props: {
+      navbarData: {
+         required: false,
+         default: function () {
+           return {
+          diyClass : 'dddd',
+          arrItems : [
+          {
+          navName: '',
+          panelHtml: ''
+          },
+          {
+          navName: '',
+          panelHtml: ''
+          }
+        ]
+        }
+         }
+       }
     },
     methods: {
       switchTab: function (e) {
---
+++ src/demos/navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <!-- S  page -->
+  <div class="page navbar">
+   <!--  <div class="page__hd">
+      <div class="page__title">Navbar</div>
+    </div> -->
+
+    <!-- S  page__bd -->
+
+    <div class="page__bd">
+
+    <!-- S Navbar模板 -->
+
+    <navbarTpl :navbarData = "navbar"></navbarTpl>
+
+    <!-- E Navbar模板 -->  
+
+    </div>
+
+    <!-- E  page__bd -->
+    
+  </div>
+<!-- E  page -->
+</template>
+<script type="text/template" id="tpl">
+</script>
+<script>
+  import navbarTpl from '@/components/navbar/navbar'
+
+  export default {
+    components: {navbarTpl},
+    data () {
+      return {
+        navbar: {
+          diyClass : 'diyClass',
+          arrItems : [
+          {
+          navName: '选项一',
+          panelHtml: '<div>1111111111111111111</div>'
+          },
+          {
+          navName: '选项二',
+          panelHtml: '<div>22222222222222</div>'
+          },
+          {
+          navName: '选项三',
+          panelHtml: '<div>33333333333333</div>'
+          }
+        ]
+        }
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style type="text/css">
+  .diyClass{
+    background: lightblue;
+  }
+</style>
